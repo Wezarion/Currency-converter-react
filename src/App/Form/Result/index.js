0f06c9fc79f83a9c576e0b7500f315a66e6e2725
Wojciech Klama <wojciechklamaczp@gmail.com>
2025-11-27 07:33:45 +0100

    readme update

--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -3,10 +3,15 @@ import "./style.css";
 export const Result = ({ result }) => (
     <p className="result">
         Po przeliczaeniu: 
-        {!!result && (
+        
+        {!!result ? (
             <strong className="result__value">
                 {result.targetAmount.toFixed(2)}&nbsp;{result.finalCurrency}
             </strong>
-        )}
+        ) :
+            <strong className="result__value">
+                Kwota
+            </strong> 
+        }
     </p>
 );
